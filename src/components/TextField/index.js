615ecd169341e6584bd408f80c137534151519dd
amrendra-rathore <amrendra-rathore@successive.tech>
2021-05-24 20:15:52 +0530

    Second Commit - Carousel Added

--- src/components/TextField/index.js
+++ src/components/TextField/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { Slider } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles((theme) => ({
